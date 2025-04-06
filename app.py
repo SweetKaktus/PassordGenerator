59081a2ac7c3f1bb2ecfc52fe7c77f4d252c3940
SweetKaktus <nathan.pintoaguila.gamedesign@gmail.com>
2025-04-06 08:14:30 +0200

    Password class created ; app creation WIP

---
+++ app.py
@@ -0,0 +1,5 @@
+import tkinter
+
+
+if __name__ == '__main__':
+    tkinter._test()
---
+++ password.py
@@ -0,0 +1,35 @@
+import random
+
+
+class Password():
+    def __init__(self,
+                 chars: str='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!:;,/.?ù%*$-+',
+                 password_generated: str=''):
+        self.chars = chars
+        self.password_generated = password_generated
+
+    def generate_password(self, password_strength: str):
+        chars: str = self.chars
+        alnum_chars: str = ''
+        match password_strength:
+            case 'WEAK':
+                for char in chars:
+                    if char.isalnum():
+                        alnum_chars += str(char)
+                for i in range(8):
+                    self.password_generated += random.choice(alnum_chars)
+            case 'MEDIUM':
+                for i in range(12):
+                    self.password_generated += random.choice(chars)
+            case 'STRONG':
+                for i in range(18):
+                    self.password_generated += random.choice(chars)
+                    return
+            case _:
+                raise ValueError
+
+
+if __name__ == '__main__':
+    test = Password()
+    test.generate_password('g')
+    print(test.password_generated)
